eeea5fc1e2819f14e41240d407ad0a89473ffded
ThomG1 <thomas.gwyther@hotmail.co.uk>
2023-04-29 09:31:26 +0000

    Stripe Part 2

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51N29e9EzmtdDooG9kbdzT3ep8cf3eXMTIbxzwruREq6xp8TGXiitFd09z2qZiWTATHrKiN4yodPaioSSrpVJGnOs007qYVBjke',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
